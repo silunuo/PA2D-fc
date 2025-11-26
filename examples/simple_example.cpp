8cd881cd6d2022069c00635b273edc12d0c5ca00
Wu <1926224891@qq.com>
2025-11-26 20:49:41 +0800

    Update simple_example.cpp

--- examples/simple_example.cpp
+++ examples/simple_example.cpp
@@ -1,13 +1,13 @@
 #include "pa2d.h"
-pa2d::Window window(600, 400, "Draw a circle"); // Create window object - 创建窗口对象
-pa2d::Canvas canvas(600, 400);                  // Create canvas object - 创建画布对象
+pa2d::Window window(600, 400, "Draw a circle");
+pa2d::Canvas canvas(600, 400);
 
 int main() {
-    window.show(); // Display the window - 显示窗口
-    canvas.circle(200, 200, 50, None, Red, 3); // Draw circle on canvas - 在画布上绘制圆
+    window.show();
+    canvas.circle(200, 200, 50, None, Red, 3);
     
     while (window.isOpen()) {
-        window.render(canvas); // Render canvas to window - 将画布渲染到窗口
+        window.render(canvas);
     }
     return 0;
 }
